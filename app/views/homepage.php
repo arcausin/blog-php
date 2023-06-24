857bb8b336d36c0e4f206a893d9e052adca6fbc9
arcausin <arcausin@gmail.com>
2023-06-24 04:40:46 +0200

    add cv homepage

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -8,7 +8,7 @@
 <h1 class="text-center mt-5 mb-5">Alexis D'Ambrosio</h1>
 <h2 class="text-center mt-5 mb-5"><strong>Développeur web PHP</strong></h2>
 <h2 class="fs-4 text-center mt-5 mb-5"><i>Explorez mon univers digital où compétences techniques, créativité et passion se rencontrent pour donner vie à des projets web exceptionnels.</i></h2>
-<p class="fs-4 text-center mt-5 mb-3"><a class="text-dark" href="/">Télécharger mon CV</a></p>
+<p class="fs-4 text-center mt-5 mb-3"><a class="text-dark" href="/public/document/cv_alexis_dambrosio.pdf" target="_blank">Télécharger mon CV</a></p>
 <p class="fs-1 text-center"><i class="fas fa-book"></i></p>
 <?php $content = ob_get_clean(); ?>
 
